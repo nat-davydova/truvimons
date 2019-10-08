cc600113409bb08b700ba8dc91d26f9d222283b9
nat-davydova <natti.davydova@gmail.com>
2019-10-08 21:55:30 +0300

    Header: nav basics

--- truvimons/src/components/Header/Header.js
+++ truvimons/src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import Navigation from './Navigation/Navigation';
+
 import classes from './Header.module.scss';
 
 const header = (props) => {
 
 	return (
 		<header className={classes.Header}>
-			
-			<nav>Navigation</nav>
+
+			<Navigation/>
 
 			<div className={classes.Header_Content}>
 				Header Content
---
+++ truvimons/src/components/Header/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Navigation.module.scss';
+
+const navigation = (props) => {
+
+	return (
+		<nav>
+
+		</nav>
+	);
+
+};
+
+export default navigation;
