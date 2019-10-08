8ca3260d924cd9bde9f0fb9e7dbd2f32323d657a
nat-davydova <natti.davydova@gmail.com>
2019-10-08 21:39:40 +0300

    Layout: basic component

--- truvimons/src/App.js
+++ truvimons/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+
+import Layout from './containers/Layout/Layout';
+
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+
+      <Layout>
+
+      </Layout>
+
   );
+
 }
 
 export default App;
---
+++ truvimons/src/containers/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+
+class Layout extends Component {
+
+	render() {
+
+		return (
+			<Fragment>
+				Text
+			</Fragment>
+		)
+
+	}
+}
+
+export default Layout;
