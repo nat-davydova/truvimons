834d3b30be41fa77127104a2241d0464ed27492b
nat-davydova <natti.davydova@gmail.com>
2019-10-09 12:48:31 +0300

    Header: nav links styles

--- truvimons/src/components/Header/Navigation/NavigationLinks/NavigationLink/NavigationLink.js
+++ truvimons/src/components/Header/Navigation/NavigationLinks/NavigationLink/NavigationLink.js
@@ -5,7 +5,8 @@ import classes from './NavigationLink.module.scss';
 const navigationLink = (props) => {
 	return(
 		<li className={classes.Nav_item}>
-			<a href={props.href}
+			<a className={classes.Nav_link}
+			   href={props.href}
 			   title={props.children}>
 				{props.children}
 			</a>
