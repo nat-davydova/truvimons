b491bd49df98b43785748cf723566eca3acc1e12
nat-davydova <natti.davydova@gmail.com>
2019-10-11 16:42:54 +0300

    Home page: basic component

---
+++ truvimons/src/views/pages/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+import Section from '../../../components/Section/Section';
+
+const home = (props) => {
+
+	return(
+
+		<Fragment>
+
+			<Section badge="Services"
+					 descr="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+Curae Suspendisse ante arcu, consectetur id nisl eget"
+					 id="services"
+					 title="Services that we provide">
+
+			</Section>
+
+		</Fragment>
+
+	);
+
+};
+
+export default home;
