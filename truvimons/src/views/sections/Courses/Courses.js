2aabc1abdae53490c43c2a6425699da045df90ef
nat-davydova <natti.davydova@gmail.com>
2019-10-14 16:26:13 +0300

    Courses: cta added, CTA: small img styles fixes

--- truvimons/src/views/sections/Courses/Courses.js
+++ truvimons/src/views/sections/Courses/Courses.js
@@ -3,10 +3,30 @@ import React from 'react';
 import Section from '../../../components/Section/Section';
 import CallToAction from '../../../components/CallToAction/CallToAction';
 
+import {ReactComponent as BusinessImage} from "../../../assets/img/busines-plan.svg";
+
 const coursesSection = (preps) => {
 
 	return(
 		<Section id="courses">
+
+			<CallToAction directionReversed={false}
+						  title="We have professional courses in real life"
+						  titleLvl="3"
+						  description="Proin maximus varius libero, dictum placerat erat pretium vitae. Praesent sed quam quis metus elementum consequat. Morbi commodo mattis leo"
+						  btns = {
+							  [
+								  {
+									  active: true,
+									  category: 'gradient',
+									  text: 'Learn More',
+									  type: 'button'
+								  }
+							  ]
+						  }
+			>
+				<BusinessImage/>
+			</CallToAction>
 
 		</Section>
 	);
