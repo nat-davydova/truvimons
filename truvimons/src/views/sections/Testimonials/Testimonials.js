28e3f35644db02b1823ec425397f5559d0ffd67e
nat-davydova <natti.davydova@gmail.com>
2019-10-15 20:38:30 +0300

    Hotfix: section descr styles fixes

--- truvimons/src/views/sections/Testimonials/Testimonials.js
+++ truvimons/src/views/sections/Testimonials/Testimonials.js
@@ -8,31 +8,39 @@ import TestImg2 from '../../../assets/img/photos/test2.jpg';
 import TestImg3 from '../../../assets/img/photos/test3.jpg';
 import TestImg4 from '../../../assets/img/photos/test4.jpg';
 
+import classes from './Testimonials.module.scss';
+
 const testimonialsSection = (props) => {
 	return(
 		<Section badge="testimonials"
+				 className={classes.testimonials}
 				 id="testimonials"
 				 title="We don't hide our clients' opinion">
 
-			<TestimonialCard descr="Maecenas a risus metus. Vivamus mollis nibh eu tellus"
-							 name="Jane Doe"
-							 src={TestImg1}
-							 title="Best Experience!"/>
+			<div className={classes.testimonials_inner}>
 
-			<TestimonialCard descr="Nulla vitae aliquam odio, ut suscipit magna. Maecenas a risus metus."
-							 name="Michael Lang"
-							 src={TestImg2}
-							 title="Nice work"/>
+				<TestimonialCard descr="Maecenas a risus metus. Vivamus mollis nibh eu tellus"
+								 name="Jane Doe"
+								 src={TestImg1}
+								 title="Best Experience!"/>
 
-			<TestimonialCard descr="Duis enim nulla, maximus at metus vitae, faucibus. Maecenas a risus metus. Vivamus."
-							 name="Linda Walker"
-							 src={TestImg3}
-							 title="I say just WOW!"/>
+				<TestimonialCard descr="Nulla vitae aliquam odio, ut suscipit magna. Maecenas a risus metus."
+								 name="Michael Lang"
+								 src={TestImg2}
+								 title="Nice work"/>
 
-			<TestimonialCard descr="Maecenas a risus metus. Vivamus mollis nibh eu tellus"
-							 name="Jonas Hays"
-							 src={TestImg4}
-							 title="Amazing result"/>
+				<TestimonialCard descr="Duis enim nulla, maximus at metus vitae, faucibus. Maecenas a risus metus. Vivamus."
+								 name="Linda Walker"
+								 src={TestImg3}
+								 title="I say just WOW!"/>
+
+				<TestimonialCard descr="Maecenas a risus metus. Vivamus mollis nibh eu tellus"
+								 name="Jonas Hays"
+								 src={TestImg4}
+								 title="Amazing result"/>
+
+			</div>
+
 		</Section>
 	);
 };
