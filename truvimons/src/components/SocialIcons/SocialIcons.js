504ec45854d966977c8aa0776c134fca81413d43
nat-davydova <natti.davydova@gmail.com>
2019-10-17 18:15:13 +0300

    Social icons: array of links

--- truvimons/src/components/SocialIcons/SocialIcons.js
+++ truvimons/src/components/SocialIcons/SocialIcons.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+import {faGooglePlay} from '@fortawesome/free-brands-svg-icons';
+
 import classes from './SocialIcons.module.scss';
 
 const socialIcons = (props) => {
+
+	const socialLinks = [
+
+		{
+			title: 'Facebook',
+			link: 'https://www.facebook.com/',
+			icon: 'FacebookF'
+		},
+
+		{
+			title: 'Twitter',
+			link: 'https://twitter.com',
+			icon: 'Twitter'
+		},
+
+		{
+			title: 'LinkedIn',
+			link: 'https://www.linkedin.com',
+			icon: 'Twitter'
+		},
+
+		{
+			title: 'Google Play',
+			link: 'https://play.google.com/store',
+			icon: 'Twitter'
+		}
+
+	];
+
 	return(
 		<div className={classes.socialIcons}>
+
 			<ul className={classes.socialIcons_list}>
 
 			</ul>
