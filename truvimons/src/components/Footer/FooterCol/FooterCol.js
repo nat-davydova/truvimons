aec1012ca3d05fd5d238c2447769d424e85131f4
nat-davydova <natti.davydova@gmail.com>
2019-10-28 17:58:07 +0300

    Refactoring: footer - syntax improvements

--- truvimons/src/components/Footer/FooterCol/FooterCol.js
+++ truvimons/src/components/Footer/FooterCol/FooterCol.js
@@ -7,20 +7,11 @@ const footerCol = (props) => {
 
 	const { title, linksList } = props;
 
-	//render column title
-	const footerTitle = title ? <Title>{title}</Title> : null;
-
-	//render links list
-	const linksListBlock = linksList ? <LinksList linksList={linksList}/> : null;
-
-	//render other content
-	const content = props.children ? props.children : null;
-
 	return(
 		<div className="footer_col">
-			{footerTitle}
-			{linksListBlock}
-			{content}
+			{title ? <Title>{title}</Title> : null}
+			{linksList ? <LinksList linksList={linksList}/> : null}
+			{props.children ? props.children : null}
 		</div> );
 };
 
