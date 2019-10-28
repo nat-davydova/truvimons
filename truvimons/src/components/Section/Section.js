ef26c819cc710f8d17ec669768989c680cf00f3b
nat-davydova <natti.davydova@gmail.com>
2019-10-28 19:08:14 +0300

    Refactoring: section - title block in a separate file

--- truvimons/src/components/Section/Section.js
+++ truvimons/src/components/Section/Section.js
@@ -2,9 +2,8 @@ import React from 'react';
 import classnames from 'classnames';
 
 import Container from '../Miscellaneous/Container/Container';
-import Badge from './Badge/Badge';
-import Title from './Title/Title';
-import Descr from './Descr/Descr';
+import TitleBlock from './TitleBlock/TitleBlock';
+
 
 import classes from './Section.module.scss';
 
@@ -12,24 +11,6 @@ const section = (props) => {
 
 	const { badge, title, descr, lightMode, paddingLarge, className, id } = props;
 
-	//rendering section title part, if it exists
-	let sectionTitleBlock;
-
-	if(badge || title || descr) {
-		const sectionBadge = badge ? <Badge>{badge}</Badge> : null;
-
-		let sectionTitle = title ? <Title lightMode={lightMode}>{title}</Title> : null;
-
-		const sectionDescr = descr ? <Descr lightMode={lightMode}>{descr}</Descr> : null;
-
-		sectionTitleBlock = <div className={classes.section_titleBlock}>
-								{sectionBadge}
-
-								{sectionTitle}
-
-								{sectionDescr}
-							</div>
-	}
 
 	let sectionClasses = classnames(
 		classes.section,
@@ -43,7 +24,10 @@ const section = (props) => {
 		<section className={sectionClasses} id={id}>
 			<Container>
 
-				{sectionTitleBlock}
+				<TitleBlock badge={badge}
+					title={title}
+					descr={descr}
+					lightMode={lightMode}/>
 
 				<div className={classes.section_content}>
 					{props.children}
