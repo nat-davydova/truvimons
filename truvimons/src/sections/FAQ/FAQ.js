d778b1a3fd3a9df876eb6344a210c4cd884a2334
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:27:26 +0300

    Refactoring: faq component - settings moved to constants.js

--- truvimons/src/sections/FAQ/FAQ.js
+++ truvimons/src/sections/FAQ/FAQ.js
@@ -3,44 +3,13 @@ import React from 'react';
 import Section from 'components/Section/Section';
 import FAQCard from 'components/FAQCard/FAQCard';
 import Button from 'components/UI/Button/Button';
+import { faqArr } from "./constants";
 
 import classes from './FAQ.module.scss';
 
 const faqSection = (props) => {
 
 	const { activeTab, faqActiveClick } = props;
-
-	const faqArr = [
-		{
-			question: 'How long are analytics stored?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		},
-
-		{
-			question: 'Can I share a report with multiple people at the same time?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		},
-
-		{
-			question: 'How often is data refreshed?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		},
-
-		{
-			question: 'What happens to my data if my social account is disconnected?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		},
-
-		{
-			question: 'Why don’t I see any data for my Facebook Page?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		},
-
-		{
-			question: 'When does data begin being collected?',
-			answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam erat laoreet.'
-		}
-	];
 
 	//rendering FAQ cards
 	const faqCards = faqArr.map(({answer, question}, index) => {
