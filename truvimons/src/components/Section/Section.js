de81d5acf50df641063a0bb288d92dd199dd421d
nat-davydova <natti.davydova@gmail.com>
2019-10-29 15:52:06 +0300

    Refactoring: section component files - aliases fixed

--- truvimons/src/components/Section/Section.js
+++ truvimons/src/components/Section/Section.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 
-import Container from '../Miscellaneous/Container/Container';
+import Container from 'components/Miscellaneous/Container/Container';
 import TitleBlock from './TitleBlock/TitleBlock';
 
 
