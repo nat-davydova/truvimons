0f8ef365eaae14b19dde0ce1f5c7883c78158e2a
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:30:41 +0300

    Refactoring: progress app component - settings moved to constants.js

--- truvimons/src/sections/ProgressApp/ProgressApp.js
+++ truvimons/src/sections/ProgressApp/ProgressApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Section from 'components/Section/Section';
 import CallToAction from 'components/CallToAction/CallToAction';
+import { btns } from "./constants";
 
 import {ReactComponent as ReportImage} from "assets/img/report.svg";
 
@@ -14,23 +15,7 @@ const progressAppSection = (preps) => {
 						  title="We have an app that show you the progress"
 						  titleLvl="3"
 						  description="Donec tempor tincidunt lacus, nec tristique nunc elementum et. Integer ac mattis massa, placerat gravida arcu."
-						  btns = {
-							  [
-								  {
-									  active: true,
-									  category: 'gradient',
-									  text: 'Learn More',
-									  type: 'button'
-								  },
-
-								  {
-									  active: false,
-									  category: 'gradient',
-									  text: 'Load App',
-									  type: 'button'
-								  }
-							  ]
-						  }
+						  btns = {btns}
 			>
 				<ReportImage/>
 			</CallToAction>
---
+++ truvimons/src/sections/ProgressApp/constants.js
@@ -0,0 +1,15 @@
+export const btns = [
+	{
+		active: true,
+		category: 'gradient',
+		text: 'Learn More',
+		type: 'button'
+	},
+
+	{
+		active: false,
+		category: 'gradient',
+		text: 'Load App',
+		type: 'button'
+	}
+];
