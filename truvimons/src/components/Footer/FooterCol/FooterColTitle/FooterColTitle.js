16aa63ee1edab2175ac85841d6ac5dc212320c86
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:39:03 +0300

    Refactoring: footer component - syntax improvements

--- truvimons/src/components/Footer/FooterCol/FooterColTitle/FooterColTitle.js
+++ truvimons/src/components/Footer/FooterCol/FooterColTitle/FooterColTitle.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import classes from './FooterColTitle.module.scss';
 
-const footerColTitle = (props) => {
-	return (<h5 className={classes.footer_colTitle}>
+const footerColTitle = (props) => (
+	<h5 className={classes.footer_colTitle}>
 		{props.children}
-	</h5>);
-};
+	</h5>
+);
 
 export default footerColTitle;
