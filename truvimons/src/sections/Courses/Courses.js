00b8e6578f8b016d0cef6395bfcd7971c6bee838
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:24:52 +0300

    Refactoring: courses component - settings moved to constants.js

--- truvimons/src/sections/Courses/Courses.js
+++ truvimons/src/sections/Courses/Courses.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Section from 'components/Section/Section';
 import CallToAction from 'components/CallToAction/CallToAction';
+import { btns } from "./constants";
 
 import {ReactComponent as BusinessImage} from "assets/img/busines-plan.svg";
 
@@ -14,16 +15,7 @@ const coursesSection = (preps) => {
 						  title="We have professional courses in real life"
 						  titleLvl="3"
 						  description="Proin maximus varius libero, dictum placerat erat pretium vitae. Praesent sed quam quis metus elementum consequat. Morbi commodo mattis leo"
-						  btns = {
-							  [
-								  {
-									  active: true,
-									  category: 'gradient',
-									  text: 'Learn More',
-									  type: 'button'
-								  }
-							  ]
-						  }
+						  btns = {btns}
 			>
 				<BusinessImage/>
 			</CallToAction>
---
+++ truvimons/src/sections/Courses/constants.js
@@ -0,0 +1,8 @@
+export const btns = [
+	{
+		active: true,
+		category: 'gradient',
+		text: 'Learn More',
+		type: 'button'
+	}
+];
