8577165e31dedc337c4b5a23aba06fd33d8bcdce
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:14:20 +0300

    Refactoring: header component - settings moved to constants.js

--- truvimons/src/components/Header/Header.js
+++ truvimons/src/components/Header/Header.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Container from 'components/Miscellaneous/Container/Container';
 import CallToAction from "components/CallToAction/CallToAction";
 
+import { btns } from './constants';
+
 import classes from './Header.module.scss';
 
 const header = (props) => {
@@ -17,23 +19,7 @@ const header = (props) => {
 								  description="We help to create SaaS product that are innovative, differentiated
 											   with a superb User Experience, fully accessible through mobile
 											   devices. SaaS products are changing the world."
-									btns= {
-										[
-											{
-												active: true,
-												category: 'gradient',
-												text: 'Start Now',
-												type: 'button'
-											},
-
-											{
-												active: false,
-												category: 'gradient',
-												text: 'Explore More',
-												type: 'button'
-											}
-										]
-									} >
+									btns= {btns} >
 					</CallToAction>
 
 				</div>
---
+++ truvimons/src/components/Header/constants.js
@@ -0,0 +1,15 @@
+export const btns = [
+	{
+		active: true,
+		category: 'gradient',
+		text: 'Start Now',
+		type: 'button'
+	},
+
+	{
+		active: false,
+		category: 'gradient',
+		text: 'Explore More',
+		type: 'button'
+	}
+];
