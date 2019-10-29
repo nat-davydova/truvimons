6bf95dcaf2f8a79ff5c4eeda97fbbca4376a1f55
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:10:43 +0300

    Refactoring: footer component - settings moved to constants.js

--- truvimons/src/components/Footer/Footer.js
+++ truvimons/src/components/Footer/Footer.js
@@ -5,75 +5,11 @@ import Logo from 'components/Logo/Logo';
 import SocialIcons from 'components/SocialIcons/SocialIcons';
 import FooterCol from './FooterCol/FooterCol';
 
+import { productsLinks, aboutLinks, learnLinks } from './constants';
+
 import classes from './Footer.module.scss';
 
 const footer = (props) => {
-
-	const productsLinks = [
-		{
-			title: 'Business',
-			href: '#'
-		},
-
-		{
-			title: 'Compare',
-			href: '#'
-		},
-
-		{
-			title: 'Features',
-			href: '#'
-		},
-
-		{
-			title: 'Pricing',
-			href: '#'
-		}
-	];
-
-	const aboutLinks = [
-		{
-			title: 'Blog',
-			href: '#'
-		},
-
-		{
-			title: 'News',
-			href: '#'
-		},
-
-		{
-			title: 'Press Releases',
-			href: '#'
-		},
-
-		{
-			title: 'Jobs',
-			href: '#'
-		}
-	];
-
-	const learnLinks = [
-		{
-			title: 'Support',
-			href: '#'
-		},
-
-		{
-			title: 'Developers',
-			href: '#'
-		},
-
-		{
-			title: 'Customer Service',
-			href: '#'
-		},
-
-		{
-			title: 'Ger Started Guide',
-			href: '#'
-		}
-	];
 
 	return(
 
---
+++ truvimons/src/components/Footer/constants.js
@@ -0,0 +1,65 @@
+export const productsLinks = [
+	{
+		title: 'Business',
+		href: '#'
+	},
+
+	{
+		title: 'Compare',
+		href: '#'
+	},
+
+	{
+		title: 'Features',
+		href: '#'
+	},
+
+	{
+		title: 'Pricing',
+		href: '#'
+	}
+];
+
+export const aboutLinks = [
+	{
+		title: 'Blog',
+		href: '#'
+	},
+
+	{
+		title: 'News',
+		href: '#'
+	},
+
+	{
+		title: 'Press Releases',
+		href: '#'
+	},
+
+	{
+		title: 'Jobs',
+		href: '#'
+	}
+];
+
+export const learnLinks = [
+	{
+		title: 'Support',
+		href: '#'
+	},
+
+	{
+		title: 'Developers',
+		href: '#'
+	},
+
+	{
+		title: 'Customer Service',
+		href: '#'
+	},
+
+	{
+		title: 'Ger Started Guide',
+		href: '#'
+	}
+];
