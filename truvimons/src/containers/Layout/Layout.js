50897eee1778283861187a31cdf8b924ba5876dd
nat-davydova <natti.davydova@gmail.com>
2019-10-29 14:10:25 +0300

    Refactoring: all the containers files - aliases fixed

--- truvimons/src/containers/Layout/Layout.js
+++ truvimons/src/containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from 'react';
 
-import Navigation from '../../components/Navigation/Navigation';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import Navigation from 'components/Navigation/Navigation';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
 
 class Layout extends Component {
 
