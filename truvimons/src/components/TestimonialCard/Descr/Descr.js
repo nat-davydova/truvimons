5d21689ecbc4b6918062c0e815668d098dd929d8
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:45:37 +0300

    Refactoring: testimonial component - syntax improvements

--- truvimons/src/components/TestimonialCard/Descr/Descr.js
+++ truvimons/src/components/TestimonialCard/Descr/Descr.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import classes from './Descr.module.scss';
 
-const descr = (props) => {
-	return (
-		<div className={classes.testimonialCard_descr}>
-			<p>{props.children}</p>
-		</div>);
-};
+const descr = (props) => (
+
+	<div className={classes.testimonialCard_descr}>
+		<p>{props.children}</p>
+	</div>
+
+);
 
 export default descr;
--- truvimons/src/components/TestimonialCard/Name/Name.js
+++ truvimons/src/components/TestimonialCard/Name/Name.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import classes from './Name.module.scss';
 
-const testimonialCardName = (props) => {
-	return <div className={classes.testimonialCard_name}>{props.children}</div>
-};
+const testimonialCardName = (props) => <div className={classes.testimonialCard_name}>{props.children}</div>;
 
 export default testimonialCardName;
