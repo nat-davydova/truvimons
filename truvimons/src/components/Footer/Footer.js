a6aded8f40e5163d891a9d2c3dd55d749867c2d3
nat-davydova <natti.davydova@gmail.com>
2019-10-29 15:45:01 +0300

    Refactoring: footer component files - aliases fixed

--- truvimons/src/components/Footer/Footer.js
+++ truvimons/src/components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Container from '../Miscellaneous/Container/Container';
-import Logo from '../Logo/Logo';
-import SocialIcons from '../SocialIcons/SocialIcons';
+import Container from 'components/Miscellaneous/Container/Container';
+import Logo from 'components/Logo/Logo';
+import SocialIcons from 'components/SocialIcons/SocialIcons';
 import FooterCol from './FooterCol/FooterCol';
 
 import classes from './Footer.module.scss';
