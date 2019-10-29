321858eac8c37686ab7b67b54ba1cde62aef9e2e
nat-davydova <natti.davydova@gmail.com>
2019-10-29 16:19:11 +0300

    Refactoring: social icons component - settings moved to constants.js

--- truvimons/src/components/SocialIcons/SocialIcons.js
+++ truvimons/src/components/SocialIcons/SocialIcons.js
@@ -3,38 +3,11 @@ import uuid from 'uuid/v1';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import SocialIcon from './SocialIcon/SocialIcon';
+import { socialLinks } from "./constants";
 
 import classes from './SocialIcons.module.scss';
 
 const socialIcons = (props) => {
-
-	const socialLinks = [
-
-		{
-			title: 'Facebook',
-			link: 'https://www.facebook.com/',
-			icon: 'facebook-f'
-		},
-
-		{
-			title: 'Twitter',
-			link: 'https://twitter.com',
-			icon: 'twitter'
-		},
-
-		{
-			title: 'LinkedIn',
-			link: 'https://www.linkedin.com',
-			icon: 'linkedin-in'
-		},
-
-		{
-			title: 'Google Play',
-			link: 'https://play.google.com/store',
-			icon: 'google-play'
-		}
-
-	];
 
 	//rendering social links
 	const socialLinksArr = socialLinks.map(({link, title, icon}) => {
